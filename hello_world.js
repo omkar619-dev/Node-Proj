a38e358964b9fc590c5e62f1950582fb235ed33e
Omkar <omkarshendge619@gmail.com>
2024-08-17 18:23:10 +0530

    nodejs learning

---
+++ hello_world.js
@@ -0,0 +1,4 @@
+const {add,sub} = require('./math')
+console.log("Hey there im node js");
+console.log("addding value of a and b is",add(3,4));
+console.log("subtraction value of a and b is",sub(3,2));
---
+++ math.js
@@ -0,0 +1,15 @@
+
+// module.exports = {
+//     add,
+//     subtract
+// }
+// function add(a ,b){
+//     return a + b;
+// }
+
+// function subtract(a,b){
+//     return a-b;
+// }
+
+exports.add = (a,b) => {return a + b}
+exports.sub = (a,b) => a - b;
